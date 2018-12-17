f3727a9b89d80cadcc791203a0366e18f7c48f67
snoowty <tanosiienjoy@gmail.com>
2018-12-18 07:42:15 +0900

    昨日はずっとできなかったのに、寝かせたら普通にできた！

--- AOJ1129_2.cpp
+++ AOJ1129_2.cpp
@@ -5,15 +5,15 @@ using namespace std;
 int main() {
   // 入れ変えるカードを一度他のvectorに入れておくやり方
   int n,r;
-  vector<int> v;
   while(cin >> n >> r) {
     if(n == 0 && r == 0) break;
     // カードの山の生成
+    vector<int> v;
     for(int i = 0; i < n; i++) v.insert(v.begin()+0,i+1);
-    for(int k = 0; k < v.size(); k++) {
-    cout << v.at(k) << " ";
-    }
-    cout << endl;
+    // for(int k = 0; k < v.size(); k++) {
+    // cout << v.at(k) << " ";
+    // }
+    // cout << endl;
 
     for(int i = 0; i < r; i++) {
       int p,c;
@@ -27,36 +27,40 @@ int main() {
       vector<int> tmp;
       // 移動するカードをtmpに追加
       for(int j = 0; j < c; j++) tmp.push_back(v[p+j-1]);
-      for(int k = 0; k < tmp.size(); k++) {
-      cout << tmp.at(k) << " ";
-      }
-      cout << endl;
+      // for(int k = 0; k < tmp.size(); k++) {
+      // cout << tmp.at(k) << " ";
+      // }
+      // cout << endl;
 
       // 移動するカードを削除
-      for(int j = 0; j < c; j++) v.erase(v.begin() + (p+j-1));
-      for(int k = 0; k < v.size(); k++) {
-      cout << v.at(k) << " ";
-      }
-      cout << endl;
+      for(int j = 0; j < c; j++) v.erase(v.begin() + (p-1));
+      // cout << "移動カード削除：";
+      // for(int k = 0; k < v.size(); k++) {
+      // cout << v.at(k) << " ";
+      // }
+      // cout << endl;
 
       // vにtmpを追加
       int tmp_size = tmp.size();
-    //  tmp.insert(tmp.end(),v.begin(),v.end());
-      for(int k = 0; k < tmp_size; k++) {
-      v.insert(v.begin()+0,tmp.back());
-      tmp.pop_back();
-    }
-
+      //  tmp.insert(tmp.end(),v.begin(),v.end());
+      for(int j = 0; j < tmp_size; j++) {
+        v.insert(v.begin()+0,tmp.back());
+        tmp.pop_back();
+      }
+      // cout << "tmp追加：　";
+      // for(int k = 0; k < v.size(); k++) cout << v.at(k) << " ";
+      // cout << endl;
       // vector<int> v = tmp;
-       //for(int j = 0; j < tmp.size(); j++) v.insert(v.begin()+0 )
+      //for(int j = 0; j < tmp.size(); j++) v.insert(v.begin()+0 )
     }
-    for(int k = 0; k < v.size(); k++) {
-    cout << v.at(k) << endl;
-  }
+    cout << v.front() << endl; 
+    //   for(int k = 0; k < v.size(); k++) {
+    //   cout << v.at(k) << endl;
+    // }
     // 初期化
-  //  vector<int> v;
+    //  vector<int> v;
   }
-  for(int i = 0; i < v.size(); i++) {
-  cout << v.at(i) << endl;
-}
+  // for(int i = 0; i < v.size(); i++) {
+  // cout << v.at(i) << endl;
+  //}
 }
