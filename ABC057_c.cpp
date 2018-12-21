f524749db10feec6df2cbcb2f3cb75d2052df2d5
snoowty <tanosiienjoy@gmail.com>
2018-12-21 12:26:15 +0900

    Digits in Multiplication_計算量を考慮する必要があった。 int型では全ての値に対応できないので、long longを使った。 AtcoderのテストケースURL：https://www.dropbox.com/sh/arnpe0ef5wds8cv/AAAk_SECQ2Nc6SVGii3rHX6Fa?dl=0

--- ABC057_c.cpp
+++ ABC057_c.cpp
@@ -3,16 +3,17 @@
 #include <map>
 #include <algorithm>
 using namespace std;
+using ll=long long;
 
 int main() {
-  int n, a, b;
+  ll n, a, b;
   cin >> n;
 
-  vector< pair<int, int> > f;
-  for(a = 1; a < n/2; a++){
+  vector< pair<ll, ll> > f;
+  for(a = 1; a*a <= n; a++){
     if(n%a == 0) {
       b = n/a;
-      pair<int, int> p(a,b);
+      pair<ll, ll> p(a,b);
       f.push_back( p );
     }
   }
@@ -22,20 +23,28 @@ int main() {
     return 0;
   }
 
-  
-  int judge, cnt = 0;
-  vector <int> ju;
+
+  ll judge;
+  int cnt = 0;
+  vector <ll> ju;
   for(int i = 0; i < f.size(); i++){
-    // cout << f[i].first << " " << f[i].second << endl;
+     // cout << f[i].first << " " << f[i].second << endl;
     if(f[i].first > f[i].second){
       judge = f[i].first;
     }else{
       judge = f[i].second;
     }
     ju.push_back( judge );
+    // cout << judge << endl;
   }
+//   cout << "ju" << endl;
+// for(int k = 0; k < ju.size(); k++) {
+//   cout << ju[k] << endl;
+// }
+
   int ju_min = *min_element(ju.begin(), ju.end());
-  // cout << "judge: " << judge << endl;
+  // cout << "ju_min: " << ju_min << endl;
+
   while(ju_min != 0){
     ju_min = ju_min/10;
     cnt++;
