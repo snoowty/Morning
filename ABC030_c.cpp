a7a639188822f752738c1151441fe407ba2d32f1
snoowty <tanosiienjoy@gmail.com>
2018-12-30 23:57:11 +0900

    飛行機乗り_マグロ解体を見た

---
+++ ABC030_c.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+
+  int x, y;
+  cin >> x >> y;
+
+  int a[n], b[m];
+  for(int i = 0; i < n; i++) cin >> a[i];
+  for(int i = 0; i < m; i++) cin >> b[m];
+
+  int time = a[0] - x;
+  for(int i = 0; i < n+m; i = i + 2) {
+    
+  }
+}
