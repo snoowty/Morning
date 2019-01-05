e4456ef1d5f2774011527775913afee56e33e1ab
snoowty <tanosiienjoy@gmail.com>
2019-01-05 09:41:33 +0900

    The Number of Island_キューを用いた幅優先探索 やっとできた！ int型の二次元配列にスペースなしの数字をうまく代入することができなかったからstring型のvectorを使った。 queueにpairを用いることで島の座標をpushすることができた。 そして、reachedで探索したかどうかを判定した。 入力終了判定をするために１行目を他のstring型の変数にぶち込んでそれの有無をwhileの条件文に入れた。 Abort trap: 6 範囲外のメモリにアクセスした時に出るエラー

--- AOJ0067.cpp
+++ AOJ0067.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <queue>
+#include <string>
+#include <vector>
 
 #define N 12
 
@@ -7,48 +9,73 @@ using namespace std;
 
 int main() {
   // 隣接行列
-  int adjacent[N][N];
-  // 到達したかどうかを判断
-  bool reached[N][N];
-  for(int i = 0; i < N; i++) {
-    for(int j = 0; j < N; j++) {
-      cin >> adjacent[i][j];
-      reached[i][j] = false;
-    }
-  }
+  // int adjacent[N][N];
+  string s;
+  while(cin >> s) {
+    vector<string> numStr(N);
+    // 到達したかどうかを判断
+    bool reached[N][N];
 
-  // 始点の選択
-  queue<int> que;
-  for(int i = 0; i < N; i++) {
-    for(int j = 0; j < N; j++) {
-      if(reached[i][j] =que.push(adjacent[i][j]);
+    numStr[0] = s;
+    for(int i = 1; i < N; i++) {
+      cin >> numStr[i];
     }
-  }
 
-  // struct Path {
-  //   int path[N];
-  //   int len;
-  // };
-  //
-  // // 現在地点を求める
-  // int top(Path *p)
-  // {
-  //   return p->path[p->len - 1];
-  // }
-  //
-  // // 経路に頂点が含まれているか
-  // bool visited(Path *p, int x)
-  // {
-  //   for (int i = 0; i < p->len; i++) {
-  //     if (p->path[i] == x) return true;
-  //   }
-  //   return false;
-  // }
-  //
-  // // 経路の表示
-  // void print_path(Path *p)
-  // {
-  //   for (int i = 0; i < p->len; i++)
-  //     printf("%c ", 'A' + p->path[i] - 1);
-  //   printf("\n");
-  // }
+    for(int i = 0; i < N; i++) {
+      for(int j = 0; j < N; j++) {
+        // cout << numStr[i].at(j);
+        reached[i][j] = false;
+      }
+      // cout << endl;
+    }
+    // cout << numStr[N+1];
+
+    // 始点の選択
+    queue<pair<int, int> > que;
+    // 島の数
+    int cnt = 0;
+    for(int i = 0; i < N; i++) {
+      for(int j = 0; j < N; j++) {
+        if(reached[i][j] == false && numStr[i].at(j) == '1') {
+          que.push(pair<int, int>(i, j));
+          // その島のカウントをつける。
+          cnt++;
+          while(!que.empty()) {
+            // popする値を保存する。
+            int f = que.front().first;
+            int s = que.front().second;
+            // cout << "front = (" << f << "," << s << ")" << endl;
+            // popする値をtrueに変更する。
+            reached[f][s] = true;
+            que.pop();
+
+            // 右
+            if(!(s == N-1)) {
+              // cout << "log右" << endl;
+              if(reached[f][s+1] == false && numStr[f].at(s+1) == '1') que.push(pair<int, int>(f, s+1));
+            }
+            // 左
+            if(!(s == 0)) {
+              // cout << "log左" << endl;
+              if(reached[f][s-1] == false && numStr[f].at(s-1) == '1') que.push(pair<int, int>(f, s-1));
+            }
+            // 上
+            if(!(f == 0)) {
+              // cout << "log上" << endl;
+              if(reached[f-1][s] == false && numStr[f-1].at(s) == '1') que.push(pair<int, int>(f+1, s));
+            }
+            // 下
+            if(!(f == N-1)) {
+              // cout << "log下" << endl;
+              if(reached[f+1][s] == false && numStr[f+1].at(s) == '1') que.push(pair<int, int>(f+1, s));
+            }
+
+          }
+        }
+      }
+    }
+    cout << cnt << endl;
+    // 終了判定
+    // if(cin.eof()) break;
+  }
+}
